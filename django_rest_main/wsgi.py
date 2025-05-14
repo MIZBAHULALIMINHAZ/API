6d08c9f247f89f7194e2d31c9af8bd6206095c08
Minhaz <mizbahulaliminhaz350309@gmail.com>
2025-05-14 17:21:51 +0600

    Updated wsgi.py to create superuser and apply migrations

--- django_rest_main/wsgi.py
+++ django_rest_main/wsgi.py
@@ -1,29 +1,8 @@
-"""
-WSGI config for django_rest_main project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-"""
-WSGI config for django_rest_main project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
 import django
 from django.core.management import call_command
 from django.contrib.auth.models import User
+from django.core.wsgi import get_wsgi_application
 
 # Set the default settings module for the 'django' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_rest_main.settings')
@@ -41,5 +20,5 @@ try:
 except Exception as e:
     print(f"Migration failed: {e}")
 
+# Expose the WSGI callable
 application = get_wsgi_application()
-
