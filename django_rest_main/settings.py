da408c33f4a305e2d6a74c1303108b0c0724cc3c
Minhaz <mizbahulaliminhaz350309@gmail.com>
2025-05-14 16:53:41 +0600

    database

--- django_rest_main/settings.py
+++ django_rest_main/settings.py
@@ -76,12 +76,13 @@ WSGI_APPLICATION = 'django_rest_main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
 
 
 # Password validation
